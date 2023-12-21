2213feb094b1a62e1e868fb9c2e69231612231f5
KarimAldeen <MhmadKarimAldeen@gmail.com>
2023-12-21 15:11:30 +0300

    git push -u origin main

--- src/Layout/Ui/Footer.tsx
+++ src/Layout/Ui/Footer.tsx
@@ -43,23 +43,7 @@ const Footer = () => {
             </a>
           </p>
         </div>
-        <div className="col-lg-2 col-md-6">
-          <h5 className="text-white mb-3">Quick links</h5>
-          <ul className="list-unstyled text-muted">
-            <li>
-              <a href="#">Home</a>
-            </li>
-            <li>
-              <a href="#">About</a>
-            </li>
-            <li>
-              <a href="#">Get started</a>
-            </li>
-            <li>
-              <a href="#">FAQ</a>
-            </li>
-          </ul>
-        </div>
+      
         <div className="col-lg-2 col-md-6">
           <h5 className="text-white mb-3">Quick links</h5>
           <ul className="list-unstyled text-muted">
@@ -83,24 +67,7 @@ const Footer = () => {
             Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
             eiusmod tempor incididunt.
           </p>
-          <form action="#">
-            <div className="input-group mb-3">
-              <input
-                className="form-control"
-                type="text"
-                placeholder="Recipient's username"
-                aria-label="Recipient's username"
-                aria-describedby="button-addon2"
-              />
-              <button
-                className="btn btn-primary"
-                id="button-addon2"
-                type="button"
-              >
-                <i className="fas fa-paper-plane" />
-              </button>
-            </div>
-          </form>
+         
         </div>
       </div>
     </div>
