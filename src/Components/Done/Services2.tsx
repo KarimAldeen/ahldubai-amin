b677c1a711da2b70f9bc8679a6c02560d18720cb
KarimAldeen <MhmadKarimAldeen@gmail.com>
2024-01-04 14:47:39 +0300

    Done

--- src/Components/Done/Services2.tsx
+++ src/Components/Done/Services2.tsx
@@ -1,25 +1,32 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom';
 
 const Services2 = () => {
+    const navigate = useNavigate()
+    const scrollToTop = () => {
+      window.scrollTo({ top: 0, behavior: 'smooth' });
+      navigate("/services")
+    };
+  
   return (
     <div className='Services2_Section'>
         <h1>Popular Packages</h1>
         <div className='Services2_images'>
       
-       <div>
+       <div  onClick={scrollToTop}>
             <img src="../Services2/dentist.jpeg" alt="" />
             <h1>dentist</h1>
         </div>
-        <span>
+        <span onClick={scrollToTop}>
             <img src="../Services2/physician.webp" alt="" />
             <h1>physician</h1>
         </span>
-       <span>
+       <span onClick={scrollToTop}>
             <img src="../Services2/physician.webp" alt="" />
             <h1>physician</h1>
         </span>
 
-        <div>
+        <div onClick={scrollToTop}>
             <img src="../Services2/dentist.jpeg" alt="" />
             <h1>dentist</h1>
         </div>
