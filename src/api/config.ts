7f09a7ab669a30c1bc0537532019b77452c72629
ibrahimkarimeddin <ibrahimkarimeddin@gmail.com>
2024-03-17 21:38:13 +0300

    updat confgi

--- src/api/config.ts
+++ src/api/config.ts
@@ -2,12 +2,15 @@
 //  export const BaseURL = `https://etaxiapi.rayantaxi.com/`;
 // export const BaseURL = `https://etaxi.Point.net/`;
 // export const BaseURL = `https://AhlDubai-back-dev.point-dev.net/api/admin`;
-export const ImageBaseURL = `http://192.168.1.10:8000`;
+//export const ImageBaseURL = `http://192.168.1.10:8000`;
 
 
-export const BaseURL = `http://192.168.1.10:8000/api/`;
+//export const BaseURL = `http://192.168.1.10:8000/api/`;
+
+export const ImageBaseURL = `http://ahlapi.kammun.com/`;
 
 
+export const BaseURL = `http://ahlapi.kammun.com/api/`;
 const PROJECT_NAME = "Hijab"
 
 export const TOKEN_KEY = PROJECT_NAME +  "_TOKEN"
